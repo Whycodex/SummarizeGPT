294dfbc5ab5ad709486a13f4b66e23bf9ce6c728
Whycodex <neerajgupta12300@gmail.com>
2023-04-14 18:33:45 +0530

    Changed the API endpoints with TLDRTHis API

--- components/Summarize.js
+++ components/Summarize.js
@@ -1,19 +1,11 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet, TextInput, Button } from "react-native";
 import { Configuration, OpenAIApi } from "openai";
+import axios from "axios";
 
 function Summarize() {
     [enteredText, setEnteredText] = useState("");
     [summarizedText, setSummarizedText] = useState("");
-
-    const config = new Configuration({
-        apiKey: "ENTER YOUR OPENAI API KEY HERE",
-    });
-    const openai = new OpenAIApi(config);
-    
-    function createSummary(enteredText) {
-        return `summarize this paragraph ${enteredText} in less than 100 words`;
-    }
 
     function inputHandler(e) {
         setEnteredText(e.target.value);
@@ -21,24 +13,30 @@ function Summarize() {
 
     function submitHandler(e) {
         e.preventDefault();
-        openai.createCompletion({
-            model: "text-babbage-001",
-            prompt: createSummary(enteredText),
-            max_tokens: 100,
-        }).then((response) => {
-            if (response.status == 200) {
-                console.log(response);
-                setSummarizedText(response?.data?.choices[0]?.text);
-            }
-        }).catch((error) => {
-            console.log(error);
-        })
+
+        const options = {
+            method: 'POST',
+            url: 'https://tldrthis.p.rapidapi.com/v1/model/abstractive/summarize-url/',
+            headers: {
+                'content-type': 'application/json',
+                'X-RapidAPI-Key': 'YOUR API KEY HERE',
+                'X-RapidAPI-Host': 'tldrthis.p.rapidapi.com'
+            },
+            data: '{"url":"https://techcrunch.com/2019/08/12/verizon-is-selling-tumblr-to-wordpress-parent-automattic/","min_length":100,"max_length":300,"is_detailed":false}'
+        };
+
+        axios.request(options).then(function (response) {
+            console.log(response.data);
+            setSummarizedText(response.data.summary);
+        }).catch(function (error) {
+            console.error(error);
+        });
     }
 
     return (
         <View style={styles.container}>
             <TextInput placeholder="Input text to be summarized" onChange={inputHandler} value={enteredText} />
-                <Button title="Summarize" onPress={submitHandler} />
+            <Button title="Summarize" onPress={submitHandler} />
             <Text>{summarizedText}</Text>
         </View>
     );
@@ -47,9 +45,9 @@ function Summarize() {
 export default Summarize;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
 });
