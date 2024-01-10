09eae553f5ad935d0f6624b1c241e35c503036a4
Whycodex <neerajgupta12300@gmail.com>
2024-01-11 04:16:54 +0530

    Completed openAI integration

--- components/Summarize.js
+++ components/Summarize.js
@@ -1,49 +1,56 @@
 import React, { useState } from "react";
-import { Text, StyleSheet, TextInput, Button, SafeAreaView, ScrollView } from "react-native";
-import axios from "axios";
+import {
+  Text,
+  StyleSheet,
+  TextInput,
+  SafeAreaView,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
 
 function Summarize() {
   const [enteredText, setEnteredText] = useState("");
   const [summarizedText, setSummarizedText] = useState("");
-
   const summarizeText = async () => {
-    try {
-        const apiKey = 'APIKEY HERE'
-        const apiURL = 'https://api.openai.com/v1/chat/completions';
+    const apiKey = "API KEY HERE";
+    const apiURL = "https://api.openai.com/v1/chat/completions";
 
-        const response = await axios.post(
-            apiURL,
-            {
-                "model": "gpt-3.5-turbo",
-                "messages": [{"role": "user", "content": enteredText}],
-                "temperature": 0.7
-            },
-            {
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${apiKey}`
-                }
-            }
-        )
-        console.log(response.choices[0].message.content);
-        setSummarizedText(response.choices[0].message.content);
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${apiKey}`,
+      },
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: enteredText }],
+        temperature: 0.7,
+      }),
+    };
+    try {
+      const response = await fetch(apiURL, requestOptions);
+      const data = await response.json();
+      console.log(data.choices[0].message.content);
+      setSummarizedText(data.choices[0].message.content);
     } catch (error) {
-        console.log(error);
+      console.log(error);
     }
-  }
+  };
 
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView style={styles.screen}>
       <Text style={styles.title}>SummarizeGPT</Text>
-      <ScrollView style={styles.inputOutput}>
+      <ScrollView style={styles.container}>
         <TextInput
           placeholder="Please enter the text to be summarized here"
           onChangeText={setEnteredText}
           multiline
         />
       </ScrollView>
-      <Button title="Summarize" color={"#000"} onPress={summarizeText} />
-      <ScrollView style={styles.inputOutput}>
+      <TouchableOpacity style={styles.button} onPress={summarizeText}>
+        <Text style={styles.text}>Summarize</Text>
+      </TouchableOpacity>
+      <ScrollView style={styles.container}>
         <Text>{summarizedText}</Text>
       </ScrollView>
     </SafeAreaView>
@@ -53,7 +60,7 @@ function Summarize() {
 export default Summarize;
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
     margin: 20,
   },
@@ -62,11 +69,23 @@ const styles = StyleSheet.create({
     fontSize: 22,
     fontWeight: "800",
   },
-  inputOutput: {
+  container: {
     borderWidth: 1,
     height: 300,
     borderRadius: 4,
     padding: 10,
     marginVertical: 10,
   },
+  button: {
+    alignItems: 'center',
+  },
+  text: {
+    color: '#fff',
+    backgroundColor: '#000',
+    fontSize: 14,
+    fontWeight: 800,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 4
+  }
 });
